2b98d6b73669a893989262199891b421ab2e2218
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-08-23 07:45:24 -0400

    initial commit

---
+++ src/GuidsGuidsGuids.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuidsGuidsGuids.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guids = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid() };
+            foreach (var guid in guids)
+                System.Console.WriteLine($"{guid}");
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ src/GuidsGuidsGuids.Web/src/main.ts
@@ -0,0 +1,7 @@
+﻿import * as uuid from "uuid/v1";
+
+var guids: string[] = [uuid(), uuid()];
+
+for (var i = 0; i < guids.length; i++) {
+    console.log(guids[i]);
+}
